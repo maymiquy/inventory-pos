4679939b85d42c6b67ecd28fa8ae86602765d2cf
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-11-21 06:23:37 +0700

    chore: create new model customer income product

---
+++ app/Models/Income.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Income extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'customer_id',
+        'quantity',
+        'price_per_item',
+        'total_amount',
+        'purchase_date',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+    ];
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function incomes()
+    {
+        return $this->hasMany(Income::class);
+    }
+}
