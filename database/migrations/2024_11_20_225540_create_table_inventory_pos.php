4af73da593ac4b74e46bccca13ed27fc16e004b8
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-11-21 06:15:52 +0700

    feat: create new migration for schema inventory pos

---
+++ database/migrations/2024_11_20_225540_create_table_inventory_pos.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('supplier_id')->constrained('suppliers');
+            $table->integer('quantity');
+            $table->decimal('price_per_item', 10, 2);
+            $table->decimal('total_amount', 10, 2);
+            $table->dateTime('restock_date');
+            $table->timestamps();
+        });
+
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->integer('quantity');
+            $table->decimal('price_per_item', 10, 2);
+            $table->decimal('total_amount', 10, 2);
+            $table->dateTime('purchase_date');
+            $table->timestamps();
+        });
+
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->year('year');
+            $table->integer('month');
+            $table->decimal('total_income', 10, 2);
+            $table->decimal('total_expense', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+        Schema::dropIfExists('suppliers');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('incomes');
+        Schema::dropIfExists('expenses');
+        Schema::dropIfExists('reports');
+    }
+};
