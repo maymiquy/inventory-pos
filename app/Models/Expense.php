dc963c7b92bb74d3aaa74687c42d4584cb2e8ec7
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-11-25 02:51:40 +0700

    chore: create new model supplier expense report

---
+++ app/Models/Expense.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'supplier_id',
+        'quantity',
+        'price_per_item',
+        'total_amount',
+        'restock_date',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+}
