74ed943200d596a016b63d62250610255017e2a3
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-12-10 09:19:21 +0700

    feat: add new component breadcrumbs

---
+++ resources/js/Components/common/Breadcrumbs.jsx
@@ -0,0 +1,39 @@
+import { Link } from '@inertiajs/react';
+import React from 'react';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../ui/breadcrumb';
+
+const Breadcrumbs = (props) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {props.menu.map((item, index) => (
+          <React.Fragment key={index}>
+            <BreadcrumbItem>
+              {props.menu.length - 1 === index ? (
+                <BreadcrumbPage>
+                  {item.label}
+                </BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <Link to={item.href}>{item.label}</Link>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+            {index < props.menu.length - 1 && (
+              <BreadcrumbSeparator />
+            )}
+          </React.Fragment>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default Breadcrumbs;
