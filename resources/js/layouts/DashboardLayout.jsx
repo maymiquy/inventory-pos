fe211a403354965b79caa30d8e56cfb4ad7da875
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-12-29 01:08:13 +0700

    chore: add new props class name in dashbord layout & make utility format currency for dashboard page

--- resources/js/layouts/DashboardLayout.jsx
+++ resources/js/layouts/DashboardLayout.jsx
@@ -9,7 +9,7 @@ import { useStore } from '../hooks/use-store';
 import getMenu from '../lib/menu';
 import { cn } from '../lib/utils';
 
-const DashboardLayout = ({ children }) => {
+const DashboardLayout = ({ children, className }) => {
   const user = usePage().props.auth.user;
   const [title, setTitle] = useState('');
   const [breadcrumbList, setBreadcrumbList] = useState([]);
@@ -66,10 +66,17 @@ const DashboardLayout = ({ children }) => {
           )}
         >
           <Navbar title={title} user={user} />
-          <div className="container space-y-2 px-4 py-6 sm:px-8 md:space-y-4">
+          <div className="space-y-2 px-4 py-6 sm:px-8 md:space-y-4">
             <Breadcrumbs menu={breadcrumbList} />
-            <section className="border-px flex min-h-[calc(100vh-56px-64px-20px-24px-56px-48px)] flex-col items-center justify-center gap-2 rounded-md bg-zinc-50 shadow-md">
-              <div className="flex w-full flex-row ps-6 pt-8 text-start">
+            <section
+              className={cn(
+                'border-px flex min-h-[calc(100vh-56px-64px-20px-24px-56px-48px)] flex-col items-center justify-center gap-2 rounded-md bg-zinc-50 shadow-md',
+                className
+              )}
+            >
+              <div
+                className={`${title === 'Dashboard' && 'hidden'} flex w-full flex-row ps-6 pt-8 text-start`}
+              >
                 <h1 className="text-2xl font-bold">
                   {title}
                 </h1>
